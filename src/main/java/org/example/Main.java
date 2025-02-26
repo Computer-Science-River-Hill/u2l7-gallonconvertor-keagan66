d8ede23f1bc0a2a764d1449f4146b52363d5e435
keagan66 <keagan6@icloud.com>
2025-02-26 17:08:28 +0000

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,21 @@
-package org.example; //DO NOT DELETE
+package org.example;
+import java.text.NumberFormat;
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   System.out.print("Enter the number of gallons: ");
+   double A = scanner.nextInt();
+   NumberFormat decimal = NumberFormat.getNumberInstance();
+   System.out.println("In " + decimal.format(A) + " gallons there are:");
+   double B = A*4.0;
+   double C = B*2.0;
+   double D = C*2.0;
+   double E = D*16.0;
+   System.out.println(decimal.format(B) + " quarts");
+   System.out.println(decimal.format(C) + " pints");
+   System.out.println(decimal.format(D) + " cups");
+   System.out.println(decimal.format(E) + " tablespoons");
+ }
+}
+
